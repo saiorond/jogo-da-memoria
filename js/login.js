f73e4fd8842cc51b419809d79afdaddc4d4acf7d
Débora <d.saioron@hotmail.com>
2022-09-24 18:23:06 -0300

    salvando envio de formulario no localStorage

--- js/login.js
+++ js/login.js
@@ -1,13 +1,20 @@
 const input = document.querySelector(".login_input"); 
 const botao = document.querySelector(".login_button");
+const form = document.querySelector(".container")
 
 const validarInput = ({target}) => {
     if (target.value.length > 2) {
         botao.removeAttribute('disabled')
-    } else {
-        botao.setAttribute('disabled', '')
-    }
+        return
+    } 
+        
+    botao.setAttribute('disabled', '')
+}
+
+const eviandoFormulario = (event) => {
+    event.preventDefault();
+    localStorage.setItem('player', input.value);
 }
 
 input.addEventListener("input", validarInput);
-console.log(validarInput)
+form.addEventListener("submit", eviandoFormulario)
